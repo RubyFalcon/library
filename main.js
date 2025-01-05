4f2db1ac34a005741ebe9b92e454c0293f475b6b
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2025-01-05 16:47:16 +0000

    dom model added to page

--- main.js
+++ main.js
@@ -1,5 +1,75 @@
 const myLibrary  = [];
 
-function Book() {
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages){
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+}
+
+
+addBookToLibrary("Christine farts",  "Tupay", 4);
+
+
+const bookContainer = document.querySelector(".books-container");
+
+
+for (item of myLibrary){
+    const book = document.createElement("div");
+    book.className = "book";
+
+    const bookTitle = document.createElement("div");
+    bookTitle.className = "book-title flex";
+
+
+    const titleHeading = document.createElement('h4');
+    titleHeading.textContent = "Title: "
+    const paragraph = document.createElement('p')
+    paragraph.textContent = item.title;
+
+    bookTitle.appendChild(titleHeading);
+    bookTitle.appendChild(paragraph);
+
+    const author = document.createElement("div");
+    author.className = "author flex";
+
+    const authorHeading = document.createElement("h4");
+    authorHeading.textContent = "Author:"
+
+    authorParagraph = document.createElement("p")
+    authorParagraph.textContent = item.author;
     
+    author.appendChild(authorHeading);
+    author.appendChild(authorParagraph);
+
+    const pages = document.createElement("div");
+    pages.className = "pages flex";
+
+    const pagesHeading = document.createElement("h4");
+    pagesHeading.textContent = "Pages: "
+
+    pagesText = document.createElement("p");
+    pagesText.textContent = item.pages;
+
+    pages.appendChild(pagesHeading);
+    pages.appendChild(pagesText);
+
+
+
+    
+
+
+   
+
+    book.appendChild(bookTitle);
+    book.appendChild(author);
+    book.appendChild(pages)
+
+
+
+    bookContainer.appendChild(book)
 }
